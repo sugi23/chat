a1881da0248be5d4003fcf78d0eee74524e9a949
sugi23 <sugi23l4game@outlook.jp>
2018-11-12 15:20:46 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+
+<h1>ログイン</h1>
+<form action="chat.php">
+	<input type="text" name="uname">
+	<button>ログイン</button>
+</form>
+
+</body>
+</html>
