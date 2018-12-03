0bedac1a785b035ad02700e8d4222dc1f0692fca
sugi23 <sugi23l4game@outlook.jp>
2018-12-03 13:53:38 +0900

    set/getをAPI化

--- get.php
+++ get.php
@@ -1,15 +1,34 @@
 <?php
+require("lib.php");
+
 $result = [];
 
-$fp = fopen("data.txt", "r");
-while( ($buff=fgets($fp)) != false ){
-	$line = explode("\t", $buff);
-	$result[] = [
-		  "name"    => $line[0]
-		, "message" => $line[1]
-		, "time"    => $line[2]
+//-------------------------------------------------
+//準備
+//-------------------------------------------------
+// 実行したいSQL
+$sql = 'SELECT * FROM log';
+
+//-------------------------------------------------
+//SQLを実行
+//-------------------------------------------------
+$dbh = connectDB();   //接続
+$sth = $dbh->prepare($sql);         //SQL準備
+$sth->execute();                    //実行
+
+//取得した内容を表示する
+while(true){
+    //ここで1レコード取得
+    $buff = $sth->fetch(PDO::FETCH_ASSOC);
+    if( $buff === false ){
+        break;    //データがもう存在しない場合はループを抜ける
+    }
+    
+    $result[] = [
+    	  "name"    => $buff["name"]
+		, "message" => $buff["message"]
+		, "time"    => $buff["time"]
 	];
 }
-fclose($fp);
 
 echo json_encode($result);
--- set.php
+++ set.php
@@ -1,13 +1,23 @@
 <?php
+require("lib.php");
+
 $uname = $_POST["uname"];
 $msg   = $_POST["msg"];
 $time  = time();
 
-$fp = fopen("data.txt", "a");
-flock($fp, LOCK_EX);
-fwrite($fp, $uname."\t".$msg."\t".$time."\n");
-flock($fp, LOCK_UN);
-fclose($fp);
+//-------------------------------------------------
+//準備
+//-------------------------------------------------
+// 実行したいSQL
+$sql = 'INSERT INTO log(name,message,time) VALUES(?,?,?)';
+
+//-------------------------------------------------
+//SQLを実行
+//-------------------------------------------------
+$dbh = connectDB();                 //接続
+$sth = $dbh->prepare($sql);         //SQL準備
+$sth->execute([$uname,$msg,date("Y-m-d H:i:s",$time)]);  //実行
+
 
 echo json_encode([
 	"status"=>true
